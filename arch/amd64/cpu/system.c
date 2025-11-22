58b42e34ad0ddadd5026da2b8b363742a309c470
presbytarianpetergriffin69 <laughfactorybling@proton.me>
2025-11-21 18:09:51 -0600

    kprint modifications

--- arch/amd64/cpu/system.c
+++ arch/amd64/cpu/system.c
@@ -13,11 +13,58 @@ void kprint(const char *s)
     while (*s) console_putc(*s++);
 }
 
-static void kprint_hex64(uint64_t n)
+static void kprint_hex64(uint64_t v, int width, int uppercase)
 {
-    static const char *hex = "0123456789ABCDEF";
-    for (int i = 60; i >= 0; i -=4)
-        console_putc(hex[(n >> i) & 0xF]);
+    const char *digits = uppercase ? "0123456789ABCDEF"
+                                   : "0123456789abcdef";
+    char buf[32];
+    int i = 0;
+
+    do {
+        buf[i++] = digits[v & 0xF];
+        v >>= 4;
+    } while (v && i < (int)sizeof(buf));
+
+    while (i < width && i < (int)sizeof(buf))
+        buf[i++] = '0';
+
+    while (i--)
+        console_putc(buf[i]);
+}
+
+static void kprint_dec_signed(long long v)
+{
+    char buf[32];
+    int i = 0;
+    int neg = 0;
+
+    if (v < 0) {
+        neg = 1;
+        v = -v;
+    }
+
+    do {
+        buf[i++] = '0' + (v % 10);
+        v /= 10;
+    } while (v && i < (int)sizeof(buf));
+
+    if (neg)
+        buf[i++] = '-';
+
+    while (i--) console_putc(buf[i]);
+}
+
+static void kprint_dec_unsigned(unsigned long long v)
+{
+    char buf[32];
+    int i = 0;
+
+    do {
+        buf[i++] = '0' + (v % 10);
+        v /= 10;
+    } while (v && i < (int)sizeof(buf));
+
+    while (i--) console_putc(buf[i]);
 }
 
 void kprintf(const char *fmt, ...)
@@ -25,37 +72,104 @@ void kprintf(const char *fmt, ...)
     va_list args;
     va_start(args, fmt);
 
-    while (*fmt) {
-        if (*fmt == '%') {
-            fmt++;
-
-            switch (*fmt) {
-            case 'c':
-                console_putc((char)va_arg(args, int));
-                break;
-            case 's':
-                kprint(va_arg(args, const char*));
-                break;
-            case 'x':
-            case 'p':
-                kprint_hex64(va_arg(args, uint64_t));
-                break;
-            case '%':
-                console_putc('%');
-                break;
-            default:
-                console_putc('?');
-            }
-        } else {
-            console_putc(*fmt);
+    for (const char *p = fmt; *p; p++) {
+        if (*p != '%') {
+            console_putc(*p);
+            continue;
         }
 
-        fmt++;
+        p++/
+
+        int zero_pad = 0;
+        int width    = 0;
+        int long_count = 0;
+
+        if (*p == '0') {
+            zero_pad = 1;
+            p++;
+        }
+
+        while (*p >= '0' && *p <= '9') {
+            width = width * 10 + (*p - '0');
+            p++;
+        }
+
+        while (*p == 'l') {
+            long_count++;
+            p++;
+        }
+
+        switch (*p) {
+        case 'c': {
+            int c = va_arg(args, int);
+            console_putc((char)c);
+            break;
+        }
+        case 's': {
+            const char *s = va_arg(args, const char *);
+            if (!s) s = "(null)";
+            while (*s)
+                console_putc(*s++);
+            break;
+        }
+        case 'd':
+        case 'i': {
+            long long v;
+            if (long_count >= 2)
+                v = va_arg(args, long long);
+            else if (long_count == 1)
+                v = va_arg(args, long);
+            else
+                v = va_arg(args, int);
+            kprint_dec_signed(v);
+            break;
+        }
+        case 'u': {
+            unsigned long long v;
+            if (long_count >= 2)
+                v = va_arg(args, unsigned long long);
+            else if (long_count == 1)
+                v = va_arg(args, unsigned long);
+            else
+                v = va_arg(args, unsigned int);
+            kprint_dec_unsigned(v);
+            break;
+        }
+        case 'x':
+        case 'X': {
+            unsigned long long v;
+            if (long_count >= 2)
+                v = va_arg(args, unsigned long long);
+            else if (long_count == 1)
+                v = va_arg(args, unsigned long);
+            else
+                v = va_arg(args, unsigned int);
+
+            if (width == 0) width = 1;
+            kprint_hex64((uint64_t)v, width, *p == 'X');
+            break;
+        }
+        case 'p': {
+            void *ptr = va_arg(args, void *);
+            uint64_t v = (uint64_t)(uintptr_t)ptr;
+            kprint("0x");
+            kprint_hex64(v, 16, 0);
+            break;
+        }
+        case '%':
+            console_putc('%');
+            break;
+
+        default:
+            // Unknown specifier: print literally
+            console_putc('%');
+            console_putc(*p);
+            break;
+        }
     }
 
     va_end(args);
 }
-
 void klog(const char *s) {
     serial_puts(s);
 }
--- include/sys/acpi.h
+++ include/sys/acpi.h
@@ -3,83 +3,26 @@
 #include <stdint.h>
 #include <system.h>
 
-struct acpi_rsdp
-{
-    char signature[8];
-    uint8_t checksum;
-    char oem_id[6];
-    uint8_t revision;
-    uint32_t rsdt_address;
+/**
+ * simple implementation for now,
+ * will likely need revision down the line
+ */
 
-    // acpi 2.0 parameters
-    uint32_t length;
-    uint64_t xsdt_address;
-    uint8_t extended_checksum;
+ typedef struct XSDP_t
+ {
+    char Signature[8];
+    uint8_t Checksum;
+    char OEMID[6];
+    uint8_t Revision;
+    uint32_t RsdtAddress;
+
+    // acpi 2.0 fields
+    uint32_t Length;
+    uint64_t XsdtAddress;
+    uint8_t ExtendedChecksum;
     uint8_t reserved[3];
-} PACKED;
+ } PACKED XSDP_t;
 
-struct acpi_sdt_header
-{
-    char signature[4];
-    uint32_t length;
-    uint8_t revision;
-    uint8_t checksum;
-    char oem_id[6];
-    char oem_table_id[8];
-    uint32_t oem_revision;
-    uint32_t creator_id;
-    uint32_t creator_revision;
-} PACKED;
+ extern XSDP_t *g_xsdp;
 
-struct acpi_madt
-{
-    struct acpi_sdt_header header;
-    uint32_t lapic_addr;
-    uint32_t flags;
-} PACKED;
-
-struct acpi_madt_entry_header
-{
-    uint8_t type;
-    uint8_t length;
-} PACKED;
-
-enum 
-{
-    MADT_LAPIC = 0,
-    MADT_IOAPIC = 1,
-    MADT_INT_OVERRIDE = 2,
-    MADT_NMI_SRC = 3,
-    MADT_LAPIC_NMI = 4,
-    NADT_LAPIC_OVERRIDE = 5
-};
-
-struct madt_lapic
-{
-    struct acpi_madt_entry_header h;
-    uint8_t acpi_id;
-    uint8_t apic_id;
-    uint32_t flags;
-} PACKED;
-
-struct madt_ioapic
-{
-    struct acpi_madt_entry_header h;
-    uint8_t id;
-    uint8_t reserved;
-    uint32_t addr;
-    uint32_t gsi_base;
-} PACKED;
-
-struct madt_irq_override
-{
-    struct acpi_madt_entry_header h;
-    uint8_t bus;
-    uint8_t irq;
-    uint32_t gsi;
-    uint16_t flags;
-} PACKED;
-
-void acpi_init(void);
-void acpi_parse_madt(struct acpi_madt *madt);
-void acpi_parse_fadt(void *fadt);
+ void acpi_init(void);
